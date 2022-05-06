e91fb9bdd432da116aaa46d1dbe42a777c5340c7
Pablo Gago <pablure@gmail.com>
2022-05-06 22:10:47 +0200

    empezando con el back

--- src/components/FormLogin.vue
+++ src/components/FormLogin.vue
@@ -1,22 +1,56 @@
 <template>
   <div id="form">
-    <form action="#" method="post" class="form">
+    <!--<form action="#" method="post" class="form">-->  
+    <form onsubmit="event.preventDefault()">
       <div class="element">
         <label for="login">Login</label><br>
-        <input type="text" id="login" name="login" value="" class="" required autofocus/>
+        <input v-model="datosFormulario.login" type="text" id="login" name="login"  required autofocus/>
       </div>
       <div class="element">
         <label for="password">Contraseña</label><br>
-        <input type="password" id="apellidos" name="apellidos" value="" class="" required/>
+        <input v-model="datosFormulario.password" type="password" id="password" name="password" required/>
       </div>
-      <input type="submit" name="boton" value="Entrar"  id="boton" class="">
+      <input @click="logear" type="submit" name="boton" value="Entrar" id="boton">
     </form>
   </div>
 </template>
 
 <script>
+
+import axios from 'axios'
+
 export default {
-    name: 'FormLogin'
+    name: 'FormLogin',
+    data (){
+        return {
+            datosFormulario: {
+            login: '',
+            password: ''
+            }
+        }       
+    },
+    methods: {
+        async logear () {
+            const response = await axios.post('http://localhost/api/?servicio=login', {
+                login: this.datosFormulario.login,
+                password: this.datosFormulario.password
+            })
+            if (response.data.data.resultado === 'ok') {
+                console.log('Usuario Registrado')
+                this.resetInputs()
+        
+            } else {
+                if (response.data.data.resultado === 'no_ok') {
+                    console.log('Usuario no Registrado')
+                }
+            }
+        },
+        resetInputs () {
+            this.datosFormulario.login = ''
+            this.datosFormulario.password = ''
+        }
+    }
+    
 }
 </script>
 
